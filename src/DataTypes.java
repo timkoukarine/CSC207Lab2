dbd369e317461ce9c0ca71ec42e41d52b5a87cc5
Tim Koukarine <tkoukarine21@tkoukarine21.phub.net.cable.rogers.com>
2024-05-26 12:02:58 -0400

    Added main method and changed local var to long

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,14 +1,18 @@
 import java.util.List;
 
 public class DataTypes {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
     // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
-
-        int s = 0;
-        // below is a "foreach" loop which iterates through numbers
-        for (int x : numbers) {
-            s += x;
-        }
+        long s = 0L;
+        /* below is a "foreach" loop which iterates through numbers */
+        for (int x : numbers) s += x;
         return s;
     }
 }
