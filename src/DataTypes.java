5f7ecd35fc27555131ba839f51beff805965eac0
Tim Koukarine <tkoukarine21@tkoukarine21.phub.net.cable.rogers.com>
2024-05-26 12:04:10 -0400

    removed TODO comment

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -7,8 +7,6 @@ public class DataTypes {
 
     }
 
-
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
         long s = 0L;
         /* below is a "foreach" loop which iterates through numbers */
